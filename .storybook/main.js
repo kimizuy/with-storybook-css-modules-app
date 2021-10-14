97e750d57033f7a4835d7866bc0956150d4fbec2
kimizuy <kimizuyamasaki@gmail.com>
2021-10-14 23:55:38 +0900

    fix warning from storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,10 @@ const path = require('path');
 module.exports = {
   stories: ['../stories/*.stories.@(ts|tsx|js|jsx|mdx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  plugins: [
+    '@babel/plugin-proposal-private-property-in-object',
+    { loose: false },
+  ],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.scss$/,

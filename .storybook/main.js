625a111e3aeab0c31ea3c2f7b5502036313aa69c
kimizuy <kimizuyamasaki@gmail.com>
2021-10-16 01:00:49 +0900

    remove importLoaders

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,7 +11,6 @@ module.exports = {
         {
           loader: 'css-loader',
           options: {
-            importLoaders: 1,
             modules: {
               auto: true,
             },
